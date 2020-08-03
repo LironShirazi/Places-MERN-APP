10a12ca9adc5c32dbf5370a92c4d5f2ceeeb3a98
LironShirazi <lirliron2009@gmail.com>
2020-08-03 20:20:48 +0300

    update

---
+++ BACKEND/public/precache-manifest.7a6038f41d78af19a06805303714927a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e48f2a5a77f94973060f99f330805c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887d331bd7766ad3d9ad",
+    "url": "/static/css/3.d618370b.chunk.css"
+  },
+  {
+    "revision": "7e80a9f21fc66340e7f2",
+    "url": "/static/css/4.f3505a8f.chunk.css"
+  },
+  {
+    "revision": "dbf8c899ae6625d82b10",
+    "url": "/static/css/5.f28e2f2d.chunk.css"
+  },
+  {
+    "revision": "c48f415cf47df3d28f31",
+    "url": "/static/css/6.506d2bc7.chunk.css"
+  },
+  {
+    "revision": "fb3cff5a550cebd60eb8",
+    "url": "/static/css/main.e4718e4a.chunk.css"
+  },
+  {
+    "revision": "a4d9592946d8c4e23168",
+    "url": "/static/js/2.3c8b1ffa.chunk.js"
+  },
+  {
+    "revision": "887d331bd7766ad3d9ad",
+    "url": "/static/js/3.10903e69.chunk.js"
+  },
+  {
+    "revision": "7e80a9f21fc66340e7f2",
+    "url": "/static/js/4.cc28230d.chunk.js"
+  },
+  {
+    "revision": "dbf8c899ae6625d82b10",
+    "url": "/static/js/5.5e2e0c10.chunk.js"
+  },
+  {
+    "revision": "c48f415cf47df3d28f31",
+    "url": "/static/js/6.97801886.chunk.js"
+  },
+  {
+    "revision": "fb3cff5a550cebd60eb8",
+    "url": "/static/js/main.4925f37c.chunk.js"
+  },
+  {
+    "revision": "9827d9c9bae044b7ef8c",
+    "url": "/static/js/runtime-main.f05da6e7.js"
+  }
+]);
